a7e5613663a2624525407167c81d84dacfc47e8f
Oleksii Tiurenkov <oleksii_tiurenkov@epam.com>
2018-11-19 14:57:12 +0200

    Added VCLC methods

--- VCLC/VCLC/ViewController.swift
+++ VCLC/VCLC/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func loadView() {
+        super.loadView()
+        print("Load View")
+    }
+    
+    override func loadViewIfNeeded() {
+        super.loadViewIfNeeded()
+        print("Load View If needad")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("View Did Load")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View Will Appear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("Will Layout Subviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("Did Layout Subviews")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View Did Appear")
+    }
+    
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
