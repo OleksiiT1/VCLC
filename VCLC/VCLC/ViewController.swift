c68ee31e3bda2daad347aa1da024e8106b6afa9b
Oleksii Tiurenkov <oleksii_tiurenkov@epam.com>
2018-11-19 14:51:21 +0200

    Initial commit

---
+++ VCLC/VCLC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VCLC
+//
+//  Created by Oleksii Tiurenkov on 11/19/18.
+//  Copyright © 2018 Oleksii Tiurenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
